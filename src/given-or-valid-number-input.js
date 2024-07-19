9d612defa7c7e5d51e535f9ad1345b9d0c495940
Luis Masuelli <luisfmasuelli@gmail.com>
2024-07-18 21:44:09 -0500

    Enhancing the number check in the given-or-valid-number-input class.

--- src/given-or-valid-number-input.js
+++ src/given-or-valid-number-input.js
@@ -1,4 +1,38 @@
 const GivenOrValidInput = require("./given-or-valid-input");
+const hexInt = /^0x[a-fA-F0-9]+$/;
+const decInt = /^-?\d+$/;
+const float = /^-?((\d+(\.\d*)?)|(\.\d+))$/;
+
+function applyConvert(convert, v, raiseOnFail) {
+    let v_ = v;
+    if (hexInt.test(v)) {
+        v_ = BigInt(v).toString();
+    } else if (raiseOnFail && !decInt.test(v) && !float.test(v)) {
+        throw new Error(`Invalid boundary value: ${v}`);
+    }
+
+    switch(convert) {
+        case "string":
+            return v;
+        case "number":
+            return parseFloat(v_);
+        case "bigint":
+            return BigInt(v_.split(".")[0]);
+        default:
+            if (typeof convert === "function") return convert(v_);
+            throw new Error(`Unexpected convert setting: ${convert}`);
+    }
+}
+
+function checkRange(convert, v, min, max) {
+    try {
+        v = applyConvert(convert, v, false);
+    } catch {
+        return false;
+    }
+
+    return (min === undefined || min <= v) && (v <= max || max === undefined);
+}
 
 /**
  * An input that takes a given value and/or asks and validates
@@ -6,11 +40,22 @@ const GivenOrValidInput = require("./given-or-valid-input");
  * will the (valid) result be processed.
  */
 class GivenOrValidNumberInput extends GivenOrValidInput {
-    constructor({integerOnly, allowHex, convert, ...options}) {
+    constructor({integerOnly, allowHex, convert, min, max, ...options}) {
+        if (convert === "string" && (min !== undefined || max !== undefined)) {
+            throw new Error("Cannot use min or max boundaries where the convert is string");
+        }
+        min = min !== undefined ? applyConvert(convert, min, true) : undefined;
+        max = max !== undefined ? applyConvert(convert, max, true) : undefined;
+
         super({
             ...options, validate: (v) => {
-                if (allowHex && /0x[a-fA-F0-9]+/.test(v)) return true;
-                return (integerOnly ? /^\d+$/ : /^((\d+(\.\d*)?)|(\.\d+))$/).test(v);
+                if (hexInt.test(v)) {
+                    return allowHex && checkRange(convert, v, min, max);
+                } else if (decInt.test(v)) {
+                    return checkRange(convert, v, min, max);
+                } else if (float.test(v)) {
+                    return !integerOnly && checkRange(convert, v, min, max);
+                }
             },
             makeInvalidInputMessage: (v) => `Invalid number: ${v}`,
             onInvalidGiven: (v) => console.error(`Invalid given number: ${v}`)
@@ -19,22 +64,7 @@ class GivenOrValidNumberInput extends GivenOrValidInput {
     }
 
     async result(v) {
-        let v_ = v;
-        if (/0x[a-fA-F0-9]+/.test(v)) {
-            v_ = BigInt(v).toString();
-        }
-
-        switch(this._convert) {
-            case "string":
-                return v;
-            case "number":
-                return parseFloat(v_);
-            case "bigint":
-                return BigInt(v_.split(".")[0]);
-            default:
-                if (typeof this._convert === "function") return this._convert(v_);
-                throw new Error(`Unexpected convert setting: ${this._convert}`);
-        }
+        return applyConvert(this._convert, v);
     }
 }
 
