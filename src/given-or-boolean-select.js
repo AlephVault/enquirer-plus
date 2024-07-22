9ec69e1582c79853590143e83b08242b039ff225
Luis Masuelli <luisfmasuelli@gmail.com>
2024-07-21 23:24:38 -0500

    Changing the preprocessing of the given value in the boolean select.

--- src/given-or-boolean-select.js
+++ src/given-or-boolean-select.js
@@ -10,8 +10,38 @@ class GivenOrBooleanSelect extends BooleanPrompt {
     }
 
     async run() {
+        if (typeof this._given === "string") {
+            this._given = this._given.toLowerCase();
+        }
+
+        switch(this._given) {
+            case true:
+            case "t":
+            case "true":
+            case "y":
+            case "yes":
+            case "1":
+            case 1:
+                this._given = true;
+                break;
+            case false:
+            case "f":
+            case "false":
+            case "n":
+            case "no":
+            case "0":
+            case 0:
+                this._given = false;
+                break;
+            default:
+                if (this._given !== undefined) {
+                    console.error(`Invalid boolean value: ${this._given}`);
+                    this._given = undefined;
+                }
+        }
+
         if (this._given !== undefined) {
-            const value = !!this._given ? "t" : "f";
+            const value = this._given ? "t" : "f";
             await this.dispatch(value);
             return this.value;
         } else {
